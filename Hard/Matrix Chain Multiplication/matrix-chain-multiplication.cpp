721fb9367b3c2e80c567e929c8bfb7f3bca18441
Himanshu Shukla <43571898+himanshushukla12@users.noreply.github.com>
2023-09-15 19:42:23 +0530

    Updated Solution - GfG to GitHub

--- Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -9,32 +9,22 @@ using namespace std;
 
 class Solution{
 public:
-int matrixChainMultiplication(int *arr,int n) {
-
-    // Create a DP table to store minimum multiplication counts
-    vector<vector<int>> dp(n, vector<int>(n, 0));
-    
-    // Fill the DP table using bottom-up approach
-    for (int chainLen = 2; chainLen < n; chainLen++) {
-        for (int i = 1; i < n - chainLen + 1; i++) {
-            int j = i + chainLen - 1;
-            dp[i][j] = INT_MAX;
-            
-            for (int k = i; k < j; k++) {
-                int cost = dp[i][k] + dp[k + 1][j] + arr[i - 1] * arr[k] * arr[j];
-                dp[i][j] = min(dp[i][j], cost);
+    int matrixMultiplication(int N, int arr[])
+    {
+        // code here
+        vector<int> v(N,INT_MAX);
+        vector<vector<int>> M(N,v);
+        for(int i=0;i<N;i++) M[i][i]=0;
+        for(int i=N-1;i>=1;--i){
+            for(int j=i+1;j<N;++j){
+                for(int k=i;k<j;++k){
+                    int temp=arr[i-1]*arr[k]*arr[j]+M[i][k]+M[k+1][j];
+                    M[i][j]=min(M[i][j],temp);
+                }
             }
         }
+        return M[1][N-1];
     }
-    
-    // The result is stored in dp[1][n-1], which represents the minimum cost of multiplication
-    return dp[1][n - 1];
-}
-    int matrixMultiplication(int n, int arr[])
-    {
-       return  matrixChainMultiplication(arr,n);
-    }
-
 };
 
 //{ Driver Code Starts.
