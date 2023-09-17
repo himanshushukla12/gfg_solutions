7d7a6496a820872782836ca59ebb88ec526868cc
Himanshu Shukla <43571898+himanshushukla12@users.noreply.github.com>
2023-09-17 14:45:14 +0530

    Updated Solution - GfG to GitHub

--- Hard/Minimum operations to convert array A to B/minimum-operations-to-convert-array-a-to-b.cpp
+++ Hard/Minimum operations to convert array A to B/minimum-operations-to-convert-array-a-to-b.cpp
@@ -8,12 +8,12 @@ using namespace std;
 
 class Solution {
   public:
+  //Note: Array B[] is sorted 
     int minInsAndDel(int a[], int b[], int n, int m) {
         vector <int> lis;
         for(int i=0; i<n; i++){
             if(binary_search(b, b+m, a[i])){
-            	auto it = 
-            	lower_bound(lis.begin(), lis.end(), a[i]);
+            	auto it=lower_bound(lis.begin(), lis.end(), a[i]);
                 if(it == lis.end()) lis.push_back(a[i]);
                 else   *it = a[i];
             }
