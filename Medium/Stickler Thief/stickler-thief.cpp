6feadae1e3b82c2f75f9fd4f45aa9fc0c75c501d
Himanshu Shukla <43571898+himanshushukla12@users.noreply.github.com>
2023-09-21 11:12:52 +0530

    Updated Solution - GfG to GitHub

--- Medium/Stickler Thief/stickler-thief.cpp
+++ Medium/Stickler Thief/stickler-thief.cpp
@@ -8,23 +8,22 @@ class Solution
 {
     public:
     //Function to find the maximum money the thief can get.
-    int FindMaxSum(int a[], int n)
+    int FindMaxSum(int arr[], int n)
     {
         // Your code here
         if(n==0) return 0;
-        if(n==1) return a[0];
-        if(n==2) return max(a[0],a[1]);
-        vector<int> dp(n);
-        dp[0]=a[0];
-        dp[1]=max(a[0],a[1]);
-        for(int i=2;i<n;i++)
-        {
-            dp[i]=max(dp[i-1],a[i]+dp[i-2]);
+        if(n==1) return arr[0];
+        if(n==2) return max(arr[0],arr[1]);
+        vector<long long> dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=arr[0];
+        dp[2]=max(arr[0],arr[1]);
+        for(int i=2;i<=n;i++){
+            dp[i]=max(arr[i-1]+dp[i-2],dp[i-1]);
         }
-        return dp[n-1];
+        return dp[n];
     }
 };
-
 
 //{ Driver Code Starts.
 int main()
