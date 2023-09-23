c3a3b46dec7966ab246e1fa0035c9fd7ed26b663
Himanshu Shukla <43571898+himanshushukla12@users.noreply.github.com>
2023-09-23 10:11:27 +0530

    Updated Solution - GfG to GitHub

--- Easy/Equilibrium Point/equilibrium-point.cpp
+++ Easy/Equilibrium Point/equilibrium-point.cpp
@@ -10,23 +10,21 @@ class Solution{
     // a: input array
     // n: size of array
     int equilibriumPoint(long long a[], int n) {
-    
         // Your code here
-        long long sum=0;
+        long long rightSum=0;
+        for(int i=0;i<n;i++)
+        rightSum+=a[i];
         long long leftSum=0;
-        for(int i=0;i<n;i++) sum+=a[i];
-        for(int i=n-1;i>=0;i--)
-        {
-            if((leftSum+a[i])==sum) return  i+1;
-            sum-=a[i];
+        for(int i=0;i<n;i++){
+            if(leftSum+a[i]==rightSum)
+            return i+1;
             leftSum+=a[i];
+            rightSum-=a[i];
         }
         return -1;
     }
 
 };
-
-
 
 //{ Driver Code Starts.
 
