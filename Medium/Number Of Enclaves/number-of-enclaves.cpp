64f035895e6cf78a4eef58ba99488fd46deb6af9
Himanshu Shukla <43571898+himanshushukla12@users.noreply.github.com>
2023-09-29 21:22:42 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number Of Enclaves/number-of-enclaves.cpp
+++ Medium/Number Of Enclaves/number-of-enclaves.cpp
@@ -10,44 +10,41 @@ using namespace std;
 
 class Solution {
   public:
-    int dx[4] = { -1 ,0 ,0, 1};
-    int dy[4] = { 0, -1, 1, 0};
+    void DFS(int i,int j,int n,int m, vector<vector<bool>> &viz, vector<vector<int>> &mat) {
+        if(i<0||i>=n||j<0||j>=m||viz[i][j]||mat[i][j]==0) return;
     
-    void dfs(vector<vector<int>>& grid, int i, int j, int& n, int& m) {
-        grid[i][j] = -1;
-        for (int d = 0; d < 4; ++d) {
-            int x = i + dx[d];
-            int y = j + dy[d];
-            if (x >= 0 && x < n && y >= 0 && y < m && grid[x][y] == 1)
-                dfs(grid, x, y, n, m);
-        }
+        viz[i][j]=true;
+        DFS(i+1,j,n,m,viz,mat);
+        DFS(i-1,j,n,m,viz,mat);
+        DFS(i,j+1,n,m,viz,mat);
+        DFS(i,j-1,n,m,viz,mat);
     }
     
-    int numberOfEnclaves(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        for (int i = 0; i < m; ++i) {
-            if (grid[0][i] == 1)
-                dfs(grid, 0, i, n, m);
-            if (grid[n - 1][i] == 1)
-                dfs(grid, n - 1, i, n, m);
+    int numberOfEnclaves(vector<vector<int>> &mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<bool>> viz(n,vector<bool>(m,false));
+    
+        for(int i=0;i<n;i++){
+            if(!viz[i][0] && mat[i][0]==1) DFS(i,0,n,m,viz,mat);
+            if(!viz[i][m-1] && mat[i][m-1]==1) DFS(i,m-1,n,m,viz,mat);
         }
-
-        for (int i = 0; i < n; ++i) {
-            if (grid[i][0] == 1)
-                dfs(grid, i, 0, n, m);
-            if (grid[i][m - 1] == 1)
-                dfs(grid, i, m - 1, n, m);
+        for(int i=0;i<m;i++){
+            if(mat[0][i]==1 && !viz[0][i]) DFS(0,i,n,m,viz,mat);
+            if(mat[n-1][i]==1 && !viz[n-1][i]) DFS(n-1,i,n,m,viz,mat);
         }
-        int cnt = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j)
-                cnt += (grid[i][j] == 1);
+    
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){  // Fix here
+                if(!viz[i][j] and mat[i][j]==1) count++;
+            }
         }
-        return cnt;
+        return count;
     }
+
 };
+
 
 
 //{ Driver Code Starts.
