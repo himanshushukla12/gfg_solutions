069fab9f303cc6997588b18c1bc9fc31214c2c27
Himanshu Shukla <43571898+himanshushukla12@users.noreply.github.com>
2024-05-12 19:16:45 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum steps to destination/minimum-steps-to-destination.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minSteps(int d,int i=0,int j=0){
+        if(i-d>=0 && (i-d)%2==0){
+            return j;
+        }
+        j++;
+        return minSteps(d,i+j,j);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int d;
+        cin >> d;
+
+        Solution ob;
+        cout << ob.minSteps(d) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
